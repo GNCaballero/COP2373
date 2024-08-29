e730e9243f0f3aab6a69736373443cf6f9a30acb
GNCaballero <gnc1914@gmail.com>
2024-08-29 15:02:17 -0400

    Initial Commit

--- Gavin_1_A.py
+++ Gavin_1_A.py
@@ -1,11 +1,12 @@
-def ticket_counter():
-    total = 20
-    tickets = 0
-    buyers = 0
+total = 20
+tickets = 0
 
+def ticket_counter(total):
+    #There are zero buyers initially
+    buyers = 0
     #Loop ends when tickets run out
-    while total >= 0:
-        tickets = input("How many tickets would you like to purchase?")
+    while total > 0:
+        tickets = int(input("How many tickets would you like to purchase?"))
         #Confirm that the selection is allowed
         if tickets <= total and tickets <= 4:
             #Subtract from the total 
@@ -14,4 +15,10 @@ def ticket_counter():
         #Display when selection is incorrect
         else:
             print("You cannot purchase that amount of tickets. Confirm that you are not trying to buy more than 4 or the remaining tickets")
-        print("There are "+total+" tickets remaining.")
+        #Display remaining tickets
+        print("There are "+str(total)+" tickets remaining.")
+    print ("All tickets sold")
+    return buyers
+buyers = ticket_counter(total)
+#Display amount of buyers
+print("There have been "+str(buyers)+" individual buyers")
